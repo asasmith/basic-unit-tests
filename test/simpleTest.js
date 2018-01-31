a82009bd7224af9c88540171a4db9479775881b5
Asa Smith <smith.asa.la@gmail.com>
2018-01-31 18:55:52 -0500

    first tests :white_check_mark:

---
+++ test/simpleTest.js
@@ -0,0 +1,21 @@
+import test from 'ava'
+
+import {log} from '../src/log'
+
+test('a function is defined', t => {
+  // .true(value)
+  // assert that `value` is `true`
+  t.true(typeof log === 'function')
+
+  // print a message contextually alongside the test
+  // more useful than console.log
+  t.log('yay, we a have function!')
+})
+
+test('returns hello world', t => {
+  // .is(value, expected)
+  // assert that 'value' === 'expected'
+  t.is(log('hello world'), 'hello world')
+})
+
+// test that arg is not a number
